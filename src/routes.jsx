9330a8874bd63aab63352b787a7a3b75c68ae5d7
Steven Almeida <pyro1357@outlook.com>
2024-04-21 20:21:49 -0400

    Updated router setup

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,3 +1,4 @@
+import App from './components/App';
 import HomePage from './components/HomePage';
 import ProductPage from './components/ProductPage';
 import ShoppingCart from './components/ShoppingCart';
@@ -6,16 +7,13 @@ import ErrorPage from './components/ErrorPage';
 const routes = [
   {
     path: '/',
-    element: <HomePage />,
+    element: <App />,
     errorElement: <ErrorPage />,
-  },
-  {
-    path: 'product/:productId',
-    element: <ProductPage />,
-  },
-  {
-    path: 'cart',
-    element: <ShoppingCart />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'product/:productId', element: <ProductPage /> },
+      { path: 'cart', element: <ShoppingCart /> },
+    ],
   },
 ];
 
